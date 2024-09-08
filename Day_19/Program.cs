0a712795c5e7d95cba605c3889266fa2157231fa
gregorgr <gregor@grajzar.info>
2024-09-09 00:09:57 +0200

    initial empty day 19

---
+++ Day_19/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day_19
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AOC 2022 Day 19: Not Enough Minerals---");
+        }
+    }
+}
